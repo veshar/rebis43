b518cbfeff8d51d97f6800a422ab2119898a35ab
Veshar <>
2022-06-13 20:57:17 -0500

    4: contador con botón

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import ItemCount from './ItemCount/ItemCount'
 
 export default function ItemListContainer() {
   return (
-    <div>ItemListContainer</div>
+    <div>
+      ItemListContainer
+      <ItemCount stock = {5} initial = {1}/>
+    </div>
+    
   )
 }
